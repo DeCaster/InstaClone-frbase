f4783dcf8b6766061985de2e4f63946df9f4924f
Murad Eyvazli <119663841+DeCaster@users.noreply.github.com>
2024-03-01 11:05:14 +0300

    Initial Commit

---
+++ InstaClone-frbase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaClone-frbase
+//
+//  Created by Murad on 1.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
